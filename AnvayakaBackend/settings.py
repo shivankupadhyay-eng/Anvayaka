46a95d60b0ada2a9ad45ff4e6f1535853d5376e4
shivankupadhyay-eng <shivankupadhyay51@gmail.com>
2025-08-22 01:59:17 +0530

    db pooling

--- AnvayakaBackend/settings.py
+++ AnvayakaBackend/settings.py
@@ -97,6 +97,13 @@ DATABASES = {
     'default': dj_database_url.config(default=os.getenv('DATABASE_URL'))
 }
 
+DATABASES['default']['OPTIONS'] = {
+    'MAX_CONNS': 20,
+    'MIN_CONNS': 5,
+}
+
+# Keep connections alive longer
+CONN_MAX_AGE = 600  # 10 minutes
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
